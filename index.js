6f3a1a8dcddb18f057dcf30723f701b161c1eae4
Hester de Tester <37218899+HesterdeTester@users.noreply.github.com>
2022-05-15 19:27:30 +0200

    added serialport logic

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const path = require('path');
 const serve = require('koa-static');
 const bodyParser = require('koa-bodyparser');
 const Router = require('koa-router');
+const { SerialPort } = require('serialport')
 
 const Api = require('./api/index');
 
 const staticDirPath = path.join(__dirname, 'dist');
 const PORT = process.env.PORT || 2222;
+const port = new SerialPort({ path: '/dev/ttyACM0', baudRate: 9600 })
 
 const server = new Koa();
 const router = new Router();
@@ -26,3 +28,18 @@ server.use(router.routes()).use(router.allowedMethods());
 server.listen(PORT, () => {
   console.log('(i) - server.listen, Port:', PORT);
 });
+
+//
+
+port.write('main screen turn on', function(err) {
+    setTimeout(()=> {port.write('hoi')}, 2000 );
+  if (err) {
+    return console.log('Error on write: ', err.message)
+  }
+  console.log('message written')
+})
+
+// Open errors will be emitted as an error event
+port.on('error', function(err) {
+  console.log('Error: ', err.message)
+})
